8cedc94d67717a88f9ba9c5f22946786fc796f8e
Davi Gledson <davigledson@outlook.com>
2024-02-28 18:56:51 -0300

    viewBinding

--- app/src/main/java/com/davigledson/travelconsuption/MainActivity.kt
+++ app/src/main/java/com/davigledson/travelconsuption/MainActivity.kt
@@ -2,10 +2,20 @@ package com.davigledson.travelconsuption
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.davigledson.travelconsuption.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+        //View -> elemento do app
+    // Binding -> ligacao com o  nosso codigo
+
+
+    }
+    fun calculate(){
+        
     }
 }
