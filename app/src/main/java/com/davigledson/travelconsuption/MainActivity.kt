8b3d6d3f0ad2f05902e419792e15d355041c1cbd
Davi Gledson <davigledson@outlook.com>
2024-02-29 08:16:49 -0300

    Evento de Click

--- app/src/main/java/com/davigledson/travelconsuption/MainActivity.kt
+++ app/src/main/java/com/davigledson/travelconsuption/MainActivity.kt
@@ -2,20 +2,39 @@ package com.davigledson.travelconsuption
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Toast
 import com.davigledson.travelconsuption.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMainBinding
+class MainActivity : AppCompatActivity(),View.OnClickListener {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         var binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         //View -> elemento do app
     // Binding -> ligacao com o  nosso codigo
+        binding.buttonCalculate.setOnClickListener(this)
+
 
 
     }
-    fun calculate(){
-        
+    private fun calculate(){
+        //Toast notification
+        Toast.makeText(this,"Voce realizou um click",Toast.LENGTH_SHORT).show()
     }
+
+    fun teste(view: View) {}
+    override fun onClick(view: View) {
+
+            if(view.id == R.id.buttonCalculate){
+                calculate()
+            }
+
+    }
+
 }
